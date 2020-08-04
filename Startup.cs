8bb13e89f964e0bd3f9e2538fe520e081f993bb7
Elisha Schwarz <eschwarz0@gmail.com>
2020-08-04 02:28:11 -0400

    add missing code for configuration DI

--- Startup.cs
+++ Startup.cs
@@ -12,12 +12,18 @@ namespace CabbieFoodz
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            // services.AddDbContext(options =>
-            // options.UseMySql());
+            services.AddDbContext<CFDbContext>(options =>
+            options.UseMySql(Configuration.GetConnectionString("CabbieFoodzDb")));
             services.AddControllersWithViews();
             services.AddRazorPages();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
